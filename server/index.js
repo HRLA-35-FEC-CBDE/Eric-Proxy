25526756c43ae675016d2f8896c9394dfab72e7c
Eric Lau <ericlaucmo@gmail.com>
2020-03-29 15:50:15 -0700

    Adjusted endpoints for server

--- server/index.js
+++ server/index.js
@@ -15,91 +15,56 @@ server.use(express.static(path.join(__dirname, '../client/dist')));
 server.post('/api/searchbar/', (req, res) => {
   axios
     .post('http://18.222.13.152:3100/api/searchbar', req.body)
-<<<<<<< HEAD
     .then(({ data }) => res.status(200).send(data).end() )
     .catch((err) => res.status(400).send(err).end() );
-=======
-    .then(({ data }) => res.status(200).send(data).end())
-    .catch((err) => res.status(400).send(err).end());
->>>>>>> c2b0026667b0ce9b2d1fdbb11a774909a69d9b7e
 });
 
 // Search bar history retrieval
 server.get('/api/searchbar/history', (req, res) => {
   axios
     .get('http://18.222.13.152:3100/api/searchbar/history')
-<<<<<<< HEAD
     .then(({ data }) => res.status(200).send(data).end() )
     .catch((err) => res.status(400).send(err).end() );
-=======
-    .then(({ data }) => res.status(200).send(data).end())
-    .catch((err) => res.status(400).send(err).end());
->>>>>>> c2b0026667b0ce9b2d1fdbb11a774909a69d9b7e
 });
 
 // Search bar history insertion
 server.post('/api/searchbar/history', (req, res) => {
   axios
     .post('http://18.222.13.152:3100/api/searchbar/history', req.body)
-<<<<<<< HEAD
-    .then(({ data }) => res.status(200).send(data).end() )
-    .catch((err) => res.status(400).send(err).end() );
-=======
     .then(({ data }) => res.status(200).send(data).end())
     .catch((err) => res.status(400).send(err).end());
->>>>>>> c2b0026667b0ce9b2d1fdbb11a774909a69d9b7e
 });
 
 // Search bar history clearing
 server.delete('/api/searchbar/history', (req, res) => {
   axios
     .delete('http://18.222.13.152:3100/api/searchbar/history')
-<<<<<<< HEAD
     .then(({ data }) => res.status(200).send(data).end() )
     .catch((err) => res.status(400).send(err).end() );
-=======
-    .then(({ data }) => res.status(200).send(data).end())
-    .catch((err) => res.status(400).send(err).end());
->>>>>>> c2b0026667b0ce9b2d1fdbb11a774909a69d9b7e
 });
 
 // Category items retrieval
 server.post('/api/navbar/', (req, res) => {
   axios
     .post('http://18.222.13.152:3100/api/navbar/', req.body)
-<<<<<<< HEAD
     .then(({ data }) => res.status(200).send(data).end() )
     .catch((err) => res.status(400).send(err).end() );
-=======
-    .then(({ data }) => res.status(200).send(data).end())
-    .catch((err) => res.status(400).send(err).end());
->>>>>>> c2b0026667b0ce9b2d1fdbb11a774909a69d9b7e
 });
 
 // Login authentication
 server.post('/api/login/', (req, res) => {
   axios
     .post('http://18.222.13.152:3100/api/login/', req.body)
-<<<<<<< HEAD
     .then(({ data }) => res.status(200).send(data).end() )
     .catch((err) => res.status(400).send(err).end() );
-=======
-    .then(({ data }) => res.status(200).send(data).end())
-    .catch((err) => res.status(400).send(err).end());
->>>>>>> c2b0026667b0ce9b2d1fdbb11a774909a69d9b7e
 });
 
 // Cart items retrieval
 server.post('/api/cart/', (req, res) => {
   axios
     .post('http://18.222.13.152:3100/api/cart/', req.body)
-<<<<<<< HEAD
     .then(({ data }) => res.status(200).send(data).end() )
     .catch((err) => res.status(400).send(err).end() );
-=======
-    .then(({ data }) => res.status(200).send(data).end())
-    .catch((err) => res.status(400).send(err).end());
->>>>>>> c2b0026667b0ce9b2d1fdbb11a774909a69d9b7e
 });
 
 // Body item retrieval
