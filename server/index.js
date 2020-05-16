86b4e3b0e537515c89e25d15adc3e463020e310a
Eric Lau <ericlaucmo@gmail.com>
2020-05-16 13:17:59 -0700

    Updated proxy

--- server/index.js
+++ server/index.js
@@ -14,23 +14,23 @@ server.use(express.static(path.join(__dirname, '../client/dist')));
 // Search bar queries
 server.post('/api/searchbar/', (req, res) => {
   axios
-    .post('http://18.222.13.152:3100/api/searchbar', req.body)
-    .then(({ data }) => res.status(200).send(data).end() )
-    .catch((err) => res.status(400).send(err).end() );
+    .post('http://localhost:3100/api/searchbar', req.body)
+    .then(({ data }) => res.status(200).send(data).end())
+    .catch((err) => res.status(400).send(err).end());
 });
 
 // Search bar history retrieval
 server.get('/api/searchbar/history', (req, res) => {
   axios
-    .get('http://18.222.13.152:3100/api/searchbar/history')
-    .then(({ data }) => res.status(200).send(data).end() )
-    .catch((err) => res.status(400).send(err).end() );
+    .get('http://localhost:3100/api/searchbar/history')
+    .then(({ data }) => res.status(200).send(data).end())
+    .catch((err) => res.status(400).send(err).end());
 });
 
 // Search bar history insertion
 server.post('/api/searchbar/history', (req, res) => {
   axios
-    .post('http://18.222.13.152:3100/api/searchbar/history', req.body)
+    .post('http://localhost:3100/api/searchbar/history', req.body)
     .then(({ data }) => res.status(200).send(data).end())
     .catch((err) => res.status(400).send(err).end());
 });
@@ -38,48 +38,48 @@ server.post('/api/searchbar/history', (req, res) => {
 // Search bar history clearing
 server.delete('/api/searchbar/history', (req, res) => {
   axios
-    .delete('http://18.222.13.152:3100/api/searchbar/history')
-    .then(({ data }) => res.status(200).send(data).end() )
-    .catch((err) => res.status(400).send(err).end() );
+    .delete('http://localhost:3100/api/searchbar/history')
+    .then(({ data }) => res.status(200).send(data).end())
+    .catch((err) => res.status(400).send(err).end());
 });
 
 // Category items retrieval
 server.post('/api/navbar/', (req, res) => {
   axios
-    .post('http://18.222.13.152:3100/api/navbar/', req.body)
-    .then(({ data }) => res.status(200).send(data).end() )
-    .catch((err) => res.status(400).send(err).end() );
+    .post('http://localhost:3100/api/navbar/', req.body)
+    .then(({ data }) => res.status(200).send(data).end())
+    .catch((err) => res.status(400).send(err).end());
 });
 
 // Login authentication
 server.post('/api/login/', (req, res) => {
   axios
-    .post('http://18.222.13.152:3100/api/login/', req.body)
-    .then(({ data }) => res.status(200).send(data).end() )
-    .catch((err) => res.status(400).send(err).end() );
+    .post('http://localhost:3100/api/login/', req.body)
+    .then(({ data }) => res.status(200).send(data).end())
+    .catch((err) => res.status(400).send(err).end());
 });
 
 // Cart items retrieval
 server.post('/api/cart/', (req, res) => {
   axios
-    .post('http://18.222.13.152:3100/api/cart/', req.body)
-    .then(({ data }) => res.status(200).send(data).end() )
-    .catch((err) => res.status(400).send(err).end() );
+    .post('http://localhost:3100/api/cart/', req.body)
+    .then(({ data }) => res.status(200).send(data).end())
+    .catch((err) => res.status(400).send(err).end());
 });
 
 // Body item retrieval
 server.get('/item/:id', (req, res) => {
   axios
     .get(`http://localhost:3002/item/${req.params.id}`)
-    .then(({ data }) => res.status(200).send(data).end() )
+    .then(({ data }) => res.status(200).send(data).end())
     .catch((err) => console.error(err));
 });
 
 // Prevent random requests
 server.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => {
-    if (err) res.status(400).send(err).end(); });
+    if (err) res.status(400).send(err).end();
+  });
 });
-
 
 server.listen(port, () => console.log('Server initialized on port:', port));
